1612fc623623c966440b6da42f4933514ed4207e
tedbrunell <tbrunell@redhat.com>
2026-05-05 11:11:12 -0400

    Improve computer AI for Tic-Tac-Toe game

--- tictactoe.py
+++ tictactoe.py
@@ -24,7 +24,40 @@ class TicTacToe:
                 self.computer_move()
                 self.check_winner()
 
+    def _check_win(self, board, player):
+        wins = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+        for a, b, c in wins:
+            if board[a] == board[b] == board[c] == player:
+                return True
+        return False
+
     def computer_move(self):
+        # 1. Try to win
+        for i in range(9):
+            if self.board[i] == " ":
+                self.board[i] = self.computer
+                if self._check_win(self.board, self.computer):
+                    self.buttons[i].config(text=self.computer)
+                    return
+                self.board[i] = " "
+
+        # 2. Block human
+        for i in range(9):
+            if self.board[i] == " ":
+                self.board[i] = self.human
+                if self._check_win(self.board, self.human):
+                    self.board[i] = self.computer
+                    self.buttons[i].config(text=self.computer)
+                    return
+                self.board[i] = " "
+
+        # 3. Take center
+        if self.board[4] == " ":
+            self.board[4] = self.computer
+            self.buttons[4].config(text=self.computer)
+            return
+
+        # 4. Take random
         available = [i for i, x in enumerate(self.board) if x == " "]
         if available:
             move = random.choice(available)
@@ -32,12 +65,14 @@ class TicTacToe:
             self.buttons[move].config(text=self.computer)
 
     def check_winner(self):
-        wins = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
-        for a, b, c in wins:
-            if self.board[a] == self.board[b] == self.board[c] and self.board[a] != " ":
-                messagebox.showinfo("Tic-Tac-Toe", f"{self.board[a]} wins!")
-                self.reset_game()
-                return True
+        if self._check_win(self.board, self.human):
+            messagebox.showinfo("Tic-Tac-Toe", "You win!")
+            self.reset_game()
+            return True
+        if self._check_win(self.board, self.computer):
+            messagebox.showinfo("Tic-Tac-Toe", "Computer wins!")
+            self.reset_game()
+            return True
         if " " not in self.board:
             messagebox.showinfo("Tic-Tac-Toe", "It's a tie!")
             self.reset_game()
